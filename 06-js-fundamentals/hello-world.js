2277c30d25c5d7717b3288d89ab1eb99d7bd2853
São Ferreira <mariacferreira93@gmail.com>
2020-12-19 13:00:26 +0000

    starting java

---
+++ 06-js-fundamentals/hello-world.js
@@ -0,0 +1,14 @@
+//alert("Hello world!"); //inline comment this shows an alert
+
+//alert(3+1+2); //the sum will be perfomed before the alert
+
+// alert("Hello again!")
+
+/*declaring a variable*/
+let message;
+
+/*assign a value to a variable */
+message = "Hello world!";
+
+alert(message);
+alert(message);
