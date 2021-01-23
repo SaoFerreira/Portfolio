a913eee3898eb9812b0666597cf67221b60d8aae
São Ferreira <mariacferreira93@gmail.com>
2021-01-23 09:07:34 +0000

    some practice on js

---
+++ 10-js-fundamentals/02-practice/fix-adition.js
@@ -0,0 +1,18 @@
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+
+a = Number(a);
+b = Number(b);
+
+//a = parseInt(a);
+//b = parseInt(b);
+
+//a = +a;
+//b = +b;
+
+//a = a * 1;
+//a = a / 1;
+//a = a - 0;
+
+alert(a + b); 
+
